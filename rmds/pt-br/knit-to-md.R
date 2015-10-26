1dc5e1f0df3867b5f7cf8faed6605ff7e59ed3e7
oswaldo <oswaldosant@gmail.com>
2015-10-25 22:02:20 -0200

    Supporting files

---
+++ rmds/pt-br/knit-to-md.R
@@ -0,0 +1,27 @@
+setwd('~/Documents/projects/programming-guide-for-the-capm-r-package/rmds/pt-br')
+library(knitr)
+library(markdown)
+rm(list = ls())
+
+## Selecting sampling units
+knit('selecting.Rmd')
+
+## Mapping sampling units
+knit('mapping.Rmd')
+
+## Calculating sample size
+knit('sampling.Rmd')
+
+## Estimating demographic characteristics
+knit('estimating.Rmd')
+
+## Building population pyramids
+knit('pyramids.Rmd')
+
+## Assessing intervention effects
+knit('assessing.Rmd')
+
+## Prioritizing interventions
+knit('prioritizing.Rmd')
+
+setwd('~/Documents/projects/programming-guide-for-the-capm-r-package')
